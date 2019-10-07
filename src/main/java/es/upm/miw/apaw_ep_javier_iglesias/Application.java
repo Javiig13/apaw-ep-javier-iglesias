2c70844d440551368951f456b6bfd3ddbdb6a5c0
Javier Iglesias <javieriglesiaslopez13@gmail.com>
2019-10-07 23:39:39 +0200

    Sonar line arguments used safely

--- src/main/java/es/upm/miw/apaw_ep_javier_iglesias/Application.java
+++ src/main/java/es/upm/miw/apaw_ep_javier_iglesias/Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConf
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class}) // Not API: /error
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);// mvn clean spring-boot:run
+        SpringApplication.run(Application.class);// mvn clean spring-boot:run
     }
 }
