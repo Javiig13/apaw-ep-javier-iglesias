cd02e9d535b6470c9ec7b663580db633bb766635
Javier Iglesias <javieriglesiaslopez13@gmail.com>
2019-10-20 21:32:15 +0200

    Cleaning Project

--- src/main/java/es/upm/miw/apaw_ep_javier_iglesias/daos/SuggestionDao.java
+++
@@ -1,7 +0,0 @@
-package es.upm.miw.apaw_ep_javier_iglesias.daos;
-
-import es.upm.miw.apaw_ep_javier_iglesias.documents.Suggestion;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SuggestionDao extends MongoRepository<Suggestion, String> {
-}
--- src/main/java/es/upm/miw/apaw_ep_javier_iglesias/daos/ThemeDao.java
+++
@@ -1,7 +0,0 @@
-package es.upm.miw.apaw_ep_javier_iglesias.daos;
-
-import es.upm.miw.apaw_ep_javier_iglesias.documents.Theme;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ThemeDao extends MongoRepository<Theme, String> {
-}
