e60e121b186291ebad7367ec4bef4321df666d5d
Javier Iglesias <javieriglesiaslopez13@gmail.com>
2019-10-21 20:19:47 +0200

    Solving Code Smell on Builder Pattern.

---
+++ src/test/java/es/upm/miw/apaw_ep_javier_iglesias/patterns/DoctorBuilderTest.java
@@ -0,0 +1,60 @@
+package es.upm.miw.apaw_ep_javier_iglesias.patterns;
+
+import es.upm.miw.apaw_ep_javier_iglesias.documents.Doctor;
+import es.upm.miw.apaw_ep_javier_iglesias.documents.Internal;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoctorBuilderTest {
+
+    private Doctor doctor;
+    private DoctorBuilder doctorBuilder;
+
+    @BeforeEach
+    void setUp() {
+        doctorBuilder = new DoctorBuilder();
+
+        List<Internal> internals = new ArrayList<Internal>() {{
+            add(new Internal("1", "Pedro", true));
+        }};
+
+        doctor = doctorBuilder
+                .withName("Dr. Fernandez")
+                .withCollegiateNumber("AAA-123")
+                .withCollegiateDate(LocalDateTime.now())
+                .withInternals(internals).build();
+        assertNotNull(doctor);
+    }
+
+    @Test
+    void withName() {
+        assertNotNull(doctor.getName());
+    }
+
+    @Test
+    void withCollegiateNumber() {
+        assertNotNull(doctor.getCollegiateNumber());
+    }
+
+    @Test
+    void withCollegiateDate() {
+        assertNotNull(doctor.getCollegiateDate());
+    }
+
+    @Test
+    void withInternals() {
+        assertFalse(0 >= doctor.getInternals().size());
+    }
+
+    @Test
+    void build() {
+        assertNotNull(doctor);
+    }
+}
